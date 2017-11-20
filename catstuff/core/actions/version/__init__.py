365c25952df13a58b5288072478edd0dab187fcc
Stanley Wu <swubro@gmail.com>
2017-11-19 18:05:35 -0800

    Bug fixes in Collection tools

---
+++ catstuff/core/actions/version/__init__.py
@@ -0,0 +1,9 @@
+import os
+from catstuff.tools.config import PluginConfig
+
+_dir = os.path.dirname(os.path.realpath(__file__))
+plugin_file = os.path.join(_dir, "version.plugin")
+config = PluginConfig.from_path(plugin_file)
+
+__version__ = config.get('version', section='Documentation', default='0.0')
+mod_name = config.get('name', section='Core')
