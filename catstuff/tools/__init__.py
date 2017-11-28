043c726bcacea9c22de2d7ad90b9f3ad0813aa4f
Stanley Wu <swubro@gmail.com>
2017-11-27 23:31:24 -0800

    Added import action

--- catstuff/tools/__init__.py
+++ catstuff/tools/__init__.py
@@ -1,21 +1,7 @@
-import os.path
-import glob
-
-# Import all files in this directory
-modules = glob.glob(os.path.dirname(__file__)+"/*.py")
-__all__ = [os.path.basename(f)[:-3] for f in modules if os.path.isfile(f) and not f.endswith('__init__.py')]
-
-# Cleanup namespace
-del glob
-del os.path
-del modules
-
-from . import *
-
-# BELOW IS TO GET PYCHARM TO STFU
 from . import argparser
 from . import common
 from . import config
+from . import config_parser
 from . import db
 from . import debug
 from . import misc
