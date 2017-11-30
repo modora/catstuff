89d3346e0bacfe6f2fec7ed50d6548e04af0405a
Stanley Wu <swubro@gmail.com>
2017-11-29 17:44:29 -0800

    code cleanup

--- catstuff/tools/__init__.py
+++ catstuff/tools/__init__.py
@@ -1,23 +1,5 @@
-def import_all_files(recursive=False):
-    from os.path import dirname, basename, isfile, join
-    import glob
-    if recursive:
-        here = join(dirname(__file__), "**/*.py")
-    else:
-        here = join(dirname(__file__), "*.py")
-    modules = glob.glob(here, recursive=recursive)
-    restricted_files = {'__init__', '_core'}
-    __all__ = [basename(f)[:-3] for f in modules if
-               isfile(f) and not any(f.endswith(name + '.py') for name in restricted_files)]
-    return __all__
-
-__all__ = import_all_files()
-from . import *
-del import_all_files
-
 from .common import *
 
-# This code is redundant, the from . import * does the same but i want the IDE to stfu
 from . import argparser
 from . import common
 from . import config
--- catstuff/tools/misc.py
+++ catstuff/tools/misc.py
@@ -1,5 +1,6 @@
 from ._core import is_path_exists_or_creatable, is_pathname_valid, is_path_creatable
 
+
 def title(name, symbol='-', border_length=100):
     border(symbol=symbol, border_length=border_length)
     print(name)
@@ -9,6 +10,3 @@ def title(name, symbol='-', border_length=100):
 def border(symbol='-', border_length=100):
     single_instance = symbol * border_length
     print(single_instance[:border_length])  # print up to the specified length
-
-
-
