9561e9a730235c3bf427f705da72f0d11908aef0
Stanley Wu <swubro@gmail.com>
2017-12-11 01:01:47 -0800

    Added sample str_method

---
+++ catstuff/core_plugins/str_methods/sample/main.py
@@ -0,0 +1,32 @@
+from catstuff.tools import plugins
+from catstuff.core_plugins.str_methods.sample.config import __version__
+
+
+class Sample(plugins.StrMethod):
+    def foo(self):
+        """ Returns 'bar'"""
+        return 'bar'
+
+    def hello(self):
+        """ Returns 'world' """
+        return 'world'
+
+    def lorem(self):
+        """ Returns 'ipsum' """
+        return 'ipsum'
+
+    def ping(self):
+        """ Returns 'pong' """
+        return 'pong'
+
+    def one(self):
+        """ Returns the integer 1"""
+        return 1
+
+    def two(self):
+        """ Returns the integer 2"""
+        return 2
+
+    def args(self, *args):
+        """ Returns the list of arguments entered"""
+        return args
