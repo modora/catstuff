593d2cfb3e85e163359829bdd17269e13921682a
Stanley Wu <swubro@gmail.com>
2017-12-16 11:22:58 -0800

    Code clarity

--- catstuff/main.py
+++ catstuff/main.py
@@ -21,6 +21,23 @@ class CoreArgParser(tools.argparser.CSArgParser):
         self.add_argument('args', nargs=argparse.REMAINDER)
 
 
+def exec_action(action, args, debug=False):
+    try:
+        action = core.vars.CSVarPool.get('manager', app='catstuff').getPluginByName(name=action, category='Action')
+        if action is None:  # plugin does not exist
+            raise core.plugins.MissingPluginException('unrecognized action {}'.format(action))
+        action.plugin_object.main(args)
+    except Exception as e:
+        if debug:
+            traceback.print_exc()
+            time.sleep(0.1)  # wait until traceback finishes printing
+            # probably a smarter way to wait but w/e
+
+            cols = shutil.get_terminal_size()[0]
+            print("-" * cols)
+            CoreArgParser().error(e, exit=True)
+
+
 def main():
     parser = CoreArgParser()
     args = parser.parse_args()
@@ -31,21 +48,7 @@ def main():
         pass
 
     core.init(config_path=args.config)
-
-    try:
-        action = core.vars.CSVarPool.get('manager', app='catstuff').getPluginByName(name=args.action, category='Action')
-        if action is None:  # plugin does not exist
-            raise core.plugins.MissingPluginException('unrecognized action {}'.format(args.action))
-        action.plugin_object.main(args.args)
-    except Exception as e:
-        if args.debug:
-            traceback.print_exc()
-            time.sleep(0.1)  # wait until traceback finishes printing
-            # probably a smarter way to wait but w/e
-
-            cols = shutil.get_terminal_size()[0]
-            print("-" * cols)
-        parser.error(e, exit=True)
+    exec_action(args.action, args.args, debug=args.debug)
 
 
 if __name__ == '__main__':
